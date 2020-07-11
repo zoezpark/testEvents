e83f28eccf247a5400cde3a5c618b0bd6ea9aaa5
Zoe Park <zoezpark@gmail.com>
2020-07-11 17:00:50 +1000

    added a date filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,41 +13,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatButtonModule} from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatCardModule} from '@angular/material/card';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTreeModule} from '@angular/material/tree';
+import { FilterPipe } from './events/filter.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-    HeaderComponent
+    HeaderComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
@@ -60,71 +32,16 @@ import {MatTreeModule} from '@angular/material/tree';
     MatNativeDateModule,
     MatInputModule,
     FormsModule,
-    MatAutocompleteModule,
-    MatBadgeModule,
-    MatBottomSheetModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatStepperModule,
     MatDatepickerModule,
-    MatDialogModule,
-    MatDividerModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
     MatInputModule,
-    MatListModule,
-    MatMenuModule,
     MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatTableModule,
-    MatTabsModule,
     MatToolbarModule,
-    MatTooltipModule,
-    MatTreeModule,
   ],
   exports: [
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatStepperModule,
     MatDatepickerModule,
-    MatDialogModule,
-    MatDividerModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
     MatInputModule,
-    MatListModule,
-    MatMenuModule,
     MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatTableModule,
-    MatTabsModule,
     MatToolbarModule,
-    MatTooltipModule,
-    MatTreeModule,
   ],
   providers: [
     EventsService,
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,26 +1,33 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Event } from './event.model';
 import { EventsService } from './events.service';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
+
 export class EventsComponent implements OnInit, OnDestroy {
   events: Event[] = [];
+  allEvents: Event[] = [];
   isLoading = false;
   form: FormGroup;
+  searchLocation: string;
   private eventsSub: Subscription;
+
+  selectedDate: string;
 
   constructor(public eventsService: EventsService) { }
 
   ngOnInit() {
     this.isLoading = false;
     this.form = new FormGroup({
-      searchKeyword: new FormControl(null, {validators: [Validators.required]})
+      searchKeyword: new FormControl(null, {validators: [Validators.required]}),
+      searchLocation: new FormControl(null, {validators: [Validators.required]})
     });
     this.events = [];
     this.eventsService.getEvents('');
@@ -29,6 +36,7 @@ export class EventsComponent implements OnInit, OnDestroy {
       .subscribe((eventData: { events: Event[]; maxEvents: number}) => {
         this.isLoading = true;
         this.events = eventData.events;
+        this.allEvents = this.events;
       });
   }
 
@@ -40,7 +48,7 @@ export class EventsComponent implements OnInit, OnDestroy {
     console.log('onGetDetail');
     this.eventsService.getEventDetail($event);
   }
-  onSearchKeyword(searchKeyword) {
+  onSearchByKeyword(searchKeyword) {
     console.log(searchKeyword);
     console.log('onSearchKeyword');
     this.eventsService.getEvents(searchKeyword);
@@ -49,17 +57,40 @@ export class EventsComponent implements OnInit, OnDestroy {
       .subscribe((eventData: { events: Event[]; maxEvents: number}) => {
         this.isLoading = true;
         this.events = eventData.events;
+        this.allEvents = this.events;
       });
   }
-  onSearchLocation(searchLocation) {
+  onSearchByLocation(searchLocation) {
     console.log(searchLocation);
     console.log('onSearchLocation');
-    this.eventsService.getEventsByLocation(searchLocation);
-    this.eventsSub = this.eventsService
-      .getEventsUpdateListener()
-      .subscribe((eventData: { events: Event[]; maxEvents: number}) => {
-        this.isLoading = true;
-        this.events = eventData.events;
-      });
+    if(searchLocation.length > 0 ) {
+      // tslint:disable-next-line: max-line-length
+      this.events = this.allEvents.filter(event => event.Location.City.toLowerCase().indexOf(searchLocation) !== -1 ||
+        event.Location.State.toLowerCase().indexOf(searchLocation) !== -1 ||
+        event.Location.Country.toLowerCase().indexOf(searchLocation) !== -1);
+    } else {
+      this.events = this.allEvents;
+    }
+  }
+
+  /*addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+    this.selectedDate = `${event.value}`;
+  }*/
+  onSearchByDate(dateEvent: MatDatepickerInputEvent<Date>) {
+    const selectedDate = `${dateEvent.value}`;
+    const test  = new Date( selectedDate ).toLocaleDateString().split(',')[0];
+
+    console.log(test);
+    console.log('onSearchByDate');
+    if (test.length > 0 ) {
+      // tslint:disable-next-line: max-line-length
+      this.events = this.allEvents.filter(event => new Date(event.Time).toLocaleDateString().indexOf(test) !== -1);
+    } else {
+      this.events = this.allEvents;
+    }
+  }
+
+  resetFilters() {
+    this.ngOnInit();
   }
 }
